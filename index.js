761a80a6c7e5090af376c33b2feac18d7d1cb19a
lostmart <lostmart@hotmail.com>
2024-10-04 14:45:39 +0200

    new objects

--- index.js
+++ index.js
@@ -2,4 +2,16 @@
 
 const firstName = "bob"
 
-console.log(firstName)
+const age = 34
+
+// typeof
+
+const userOne = {
+	firstName: "Test user one",
+	age: 40,
+	admin: false,
+}
+
+userOne.age = 41
+
+console.log(userOne)
